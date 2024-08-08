db8fbf986dd9bdee05f9f33a94f0aa8b26bf08e6
kawashimatakuya117 <t.kawashima17@gmai.com>
2024-08-08 18:33:15 +0900

    テストを行うメソッド名を何を行っているかがわかりやすい名称に go.modの更新

--- hello_test.go
+++ hello_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestHandleChatWorkAPIResponse(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World!")
